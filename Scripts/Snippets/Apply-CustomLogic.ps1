66d3dac3831cf742d27b3b60e5b7f4835a9799f2
Robin Beismann <robin@beismann.biz>
2019-03-11 20:46:50 +0100

    Create Apply-CustomLogic.ps1

---
+++ Scripts/Snippets/Apply-CustomLogic.ps1
@@ -0,0 +1,45 @@
+function Validate-CustomLogic($condition,$V1,$V2){
+    $condition = $condition.ToLower()
+
+    switch($condition){
+        "like" {
+            $string = '$V1 -like "*$V2*"'
+        }
+
+        "is" {
+            $string = '$V1 -eq $V2'
+        }
+
+        "isnot" {
+            $string = '$V1 -ne $V2'
+        }
+
+        "startswith" {
+            $string = '$V1.StartsWith($V2)'
+        }
+
+        "notstartswith" {
+            $string = '!($V1.StartsWith($V2))'
+        }
+
+        "endswith" {
+            $string = '$V1.EndsWith($V2)'
+        }
+
+        "notendswith" {
+            $string = '!($V1.EndsWith($V2))'
+        }
+    }
+
+    if(     
+            $string -and 
+            ($sb = [scriptblock]::Create($string)) -and
+            ($result = $sb.Invoke())
+     ){
+        Write-Verbose("Using Logic `"$condition`" on `"$V1`" versus `"$V2`", Result is: `"$result`"<br/>")
+        return $result
+    }else{
+        Write-Verbose("Error, executing failed.")
+        return $false
+    }
+}
