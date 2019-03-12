fbe85ea7f9a0af94f0b772a5559872cdc1e0a0bd
Robin Beismann <robin@beismann.biz>
2019-03-12 23:26:49 +0100

    Update Move-CertainOUtoEXDB.ps1

--- Scripts/Snippets/Move-CertainOUtoEXDB.ps1
+++ Scripts/Snippets/Move-CertainOUtoEXDB.ps1
@@ -1,9 +1,20 @@
 Add-PSSnapIn Microsoft.Exchange.*
-Get-Mailbox -Filter * | Where-Object {
-    $_.DistinguishedName.EndsWith("xyz")
-} | % {
-    $jobName = "Move $($_.DisplayName)"
-    Write-Host("Creating Job: $jobName")
-    New-MoveRequest -Identity $_.Id -TargetDatabase "DB07" -BatchName $jobName
 
+$DN = @{
+        "DummyDN" = "Dest-DB"
+
+    }
+    
+Get-Mailbox -Filter * -ResultSize 999999999 | % {
+    $distinguishedName = $_.distinguishedName
+    if(
+        ($db = (($DN.GetEnumerator() | Where-Object { $distinguishedName.Endswith($_.Key) }).Value)) -and
+        !(Get-MoveRequest | Where-Object { ($_.distinguishedName -eq $distinguishedName)  -and ($_.Status -ne "Completed") } ) -and
+        $_.Database -ne $db
+     ){
+        $jobName = "Move $($_.DisplayName) to $db"
+        Write-Host("Creating Job: $jobName")
+        New-MoveRequest -Identity $_.Id -TargetDatabase $db -BatchName $jobName
+
+    }
 }
