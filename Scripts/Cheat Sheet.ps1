afd15063fb2e99909fd0808581ccf88d09f22f67
Robin Beismann <robin@beismann.biz>
2020-03-19 10:37:21 +0100

    Update Cheat Sheet.ps1

--- Scripts/Cheat Sheet.ps1
+++ Scripts/Cheat Sheet.ps1
@@ -6,10 +6,10 @@ Get-WmiObject Win32_volume | Where-Object { $_.Capacity } | ForEach-Object {
     $Capacity = $_.Capacity
     #Calculate
     $usage =  $FreeSpace / $Capacity * 100
-    #Round
-    $usage = [math]::Round($usage,2)
     #Free
-    $free = [math]::Round(100 - $usage,2)
+    $free = [math]::Round($usage,2)
+    #Round
+    $usage = [math]::Round(100 - $usage,2)
 
     [PSCustomobject]@{
         Name = $_.Name
