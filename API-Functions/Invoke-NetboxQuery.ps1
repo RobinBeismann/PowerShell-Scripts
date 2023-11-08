f889c1f40e10471f37e1719dd0922c04721755aa
Robin Beismann <robin@beismann.biz>
2023-11-08 16:05:43 +0100

    Overhauled Netbox function, now properly handles UTF8

--- API-Functions/Invoke-NetboxQuery.ps1
+++ API-Functions/Invoke-NetboxQuery.ps1
@@ -13,85 +13,101 @@ function Invoke-NetboxQuery {
         $ForceSSL = $true
     )
 
-    # Check if the URI is already a fully featured URI
-    if(
-        !([regex]::Match($uri,"(http|https):\/\/.*\/api\/",[Text.RegularExpressions.RegexOptions]'IgnoreCase, CultureInvariant').Success)
-    ){            
-        if($Uri.StartsWith("/")){
-            $uri = $Uri.Substring(1)
-        }
-        $Uri = $global:api_base_url + $Uri
+    begin{
+
+        # Create an empty array to store the result.
+        $QueryResults = [System.Collections.ArrayList]@()
+
+        $PreviousProgressPreference = $ProgressPreference
+        $ProgressPreference = 'SilentlyContinue'
+        $Encoding = [System.Text.Encoding]::UTF8
     }
-
-    # If ForceSSL is set, overwrite any next page urls with https
-    if($ForceSSL){
-        $uri = $uri.Replace("http://","https://")
-    }
-
-    # Format headers.
-    $HeaderParams = @{
-        'Content-Type'  = "application/json"
-        'Authorization' = "Token $global:nbToken"
-    }
-
-    # Create an empty array to store the result.
-    $QueryResults = [System.Collections.ArrayList]@()
-
-    # Invoke REST method and fetch data until there are no pages left.
-    do {
-        # Build Parameters
-        $Params = @{
-            UseBasicParsing = $true
-            Method = $Method
-            ContentType = "application/json"
-            Uri = $Uri
-            Headers = $HeaderParams
-            ErrorAction = "Stop"
-        }
-        if($Body){
-            $Params.Body = ($Body | ConvertTo-Json)
+    process{
+        # Check if the URI is already a fully featured URI
+        if(
+            !([regex]::Match($uri,"(http|https):\/\/.*\/api\/",[Text.RegularExpressions.RegexOptions]'IgnoreCase, CultureInvariant').Success)
+        ){            
+            if($Uri.StartsWith("/")){
+                $uri = $Uri.Substring(1)
+            }
+            $Uri = $global:api_base_url + $Uri
         }
 
-        # Get Result
-        try{
-            #if($Method.ToLower() -eq "get"){
-                $Results = Invoke-RestMethod @Params
-            #}
-        }catch{
-            Write-Error -ErrorAction Stop -Message "Error at Rest Request: $($_.Exception.Message)`n`n Params:`n$($params | ConvertTo-Json)"
-        }
-        # Add results to table
-        if (
-            $Results.results
-        ) {
-            $null = $QueryResults.AddRange($Results.results)
-        # Add single result to table
-        }elseif(
-            $Results -and
-            !(
-                $Results.PSObject.Properties.Name.Contains("results") -or
-                $Results.PSObject.Properties.Name.Contains("count")
-            )
-        ){
-            $null = $QueryResults.Add($Results)
+        # If ForceSSL is set, overwrite any next page urls with https
+        if($ForceSSL){
+            $uri = $uri.Replace("http://","https://")
         }
 
-        # Process next page
-        if($Results.'next'){
-            # If ForceSSL is set, overwrite any next page urls with https
-            if($ForceSSL){
-                $uri = $Results.'next'.Replace("http://","https://")
+        # Format headers.
+        $HeaderParams = @{
+            'Content-Type'  = "application/json"
+            'Authorization' = "Token $global:nbToken"
+        }
+
+
+        # Invoke REST method and fetch data until there are no pages left.
+        do {
+            # Build Parameters
+            $Params = @{
+                UseBasicParsing = $true
+                Method = $Method
+                ContentType = "application/json"
+                Uri = $Uri
+                Headers = $HeaderParams
+                ErrorAction = "Stop"
+            }
+            if($Body){
+                $Params.Body = [System.Text.Encoding]::UTF8.GetBytes(($Body | ConvertTo-Json))
+            }
+
+            # Get Result
+            try{
+                [Microsoft.PowerShell.Commands.WebResponseObject]$RawResult = Invoke-WebRequest @Params
+                $EncodedResult = $Encoding.GetString(
+                    $RawResult.RawContentStream.ToArray()
+                )
+                $Results = ConvertFrom-Json -InputObject $EncodedResult
+            }catch{
+                Write-Error -ErrorAction Stop -Message "Error at Rest Request: $($_.Exception.Message)`n`n Params:`n$($params | ConvertTo-Json)"
+            }
+            # Add results to table
+            if (
+                $Results.results
+            ) {
+                $null = $QueryResults.AddRange($Results.results)
+            # Add single result to table
+            }elseif(
+                $Results -and
+                !(
+                    $Results.PSObject.Properties.Name.Contains("results") -or
+                    $Results.PSObject.Properties.Name.Contains("count")
+                )
+            ){
+                $null = $QueryResults.Add($Results)
+            }
+
+            # Process next page
+            if($Results.'next'){
+                # If ForceSSL is set, overwrite any next page urls with https
+                if($ForceSSL){
+                    $uri = $Results.'next'.Replace("http://","https://")
+                }else{
+                    $uri = $Results.'next'
+                }      
             }else{
-                $uri = $Results.'next'
-            }      
-        }else{
-            $uri = $null
-        }
-    # Break out of loop if we got no next page
-    } until (!($uri))
+                $uri = $null
+            }
+        # Break out of loop if we got no next page
+        } until (!($uri))
 
-    # Return the result.
-    $QueryResults
+    }
+    end{
+        # Switch Progress Preference back
+        $ProgressPreference = $PreviousProgressPreference
+
+        # Return the result.
+        $QueryResults
+    }
 }
 
 # Get Devices
